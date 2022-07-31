c2d36d91e682093d448eebdcb97acf90d2e939a4
Hyunju Yang <iam@2tle.io>
2022-08-01 05:49:37 +0900

    Initial Commit

---
+++ randomText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomText
+//
+//  Created by 2tle on 2022/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
